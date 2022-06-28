3d3d6d562a2dfed2dbc2d706be0a81ebd571452c
Tania Topciov <taniatopciov@yahoo.com>
2022-06-28 18:07:07 +0300

    Solved Filter Bug

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EventsService} from '../../services/events.service';
 import {Event, EventCategory, EventType} from '../../data/event';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-card-list',
@@ -11,7 +12,13 @@ export class CardListComponent implements OnInit {
   events: Event[];
   allEvents: Event[];
   filteredEvents: Event[];
-  categories: {label: string, type: EventCategory}[] = [{label: EventCategory.arbeitsrecht, type: EventCategory.arbeitsrecht}, {label: 'HR & RECRUITING' , type: EventCategory.hr}, {label: 'HIGHLIGHTS', type: EventCategory.highlights}, {label: 'PARTNER EVENTS', type: EventCategory.partnerevents}];
+  categories: { label: string, type: EventCategory }[] = [{
+    label: EventCategory.arbeitsrecht,
+    type: EventCategory.arbeitsrecht
+  }, {label: 'HR & RECRUITING', type: EventCategory.hr}, {
+    label: 'HIGHLIGHTS',
+    type: EventCategory.highlights
+  }, {label: 'PARTNER EVENTS', type: EventCategory.partnerevents}];
   types: EventType[] = [EventType.event, EventType.seminar, EventType.workshop];
   isTypeFilterVisible: boolean = false;
   selectedCategory: string;
@@ -51,26 +58,28 @@ export class CardListComponent implements OnInit {
 
   filterByCategory(category: EventCategory) {
     if (this.selectedCategory === category) {
-      this.events = this.allEvents.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());;
+      this.events = this.allEvents.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+      this.selectedCategory = "";
     } else {
       this.events = this.allEvents?.filter(event => event.category === category);
+      this.selectedCategory = category;
     }
     this.calculatePageCount();
     this.paginateData(0);
     this.selectedPage = 0;
-    this.selectedCategory = category;
   }
 
   filterByType(type: EventType) {
     if (this.selectedType === type) {
-      this.events = this.allEvents.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());;
+      this.events = this.allEvents.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+      this.selectedType = "";
     } else {
       this.events = this.allEvents.filter(event => event.type === type);
+      this.selectedType = type;
     }
     this.calculatePageCount();
     this.paginateData(0);
     this.selectedPage = 0;
-    this.selectedType = type;
   }
 
   sortByDate() {
@@ -82,5 +91,13 @@ export class CardListComponent implements OnInit {
       this.sorting = 'desc';
     }
     this.paginateData(0);
+  }
+
+  removeTypeFilter(type: string) {
+    this.events = this.allEvents.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+    this.calculatePageCount();
+    this.paginateData(0);
+    this.selectedPage = 0;
+    this.selectedType = type;
   }
 }
--- src/app/components/carousel-card-list/carousel-card-list.component.ts
+++ src/app/components/carousel-card-list/carousel-card-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EventsService} from "../../services/events.service";
 import {Event} from "../../data/event";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-carousel-card-list',
@@ -11,12 +12,23 @@ export class CarouselCardListComponent implements OnInit {
 
   events: Event[];
 
-  constructor(private eventService: EventsService) { }
-
-  ngOnInit(): void {
-    this.eventService.getAllEvents().subscribe(events => {
-      this.events = events;
-    })
+  constructor(private eventService: EventsService, private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    const routeType = this.route.snapshot.queryParams['type'];
+    const routeCategory = this.route.snapshot.queryParams['category'];
+
+    this.eventService.getAllEvents().subscribe(events => {
+      this.events = events;
+
+      if(routeType) {
+        this.events = this.events?.filter(event => event.type === routeType);
+      }
+
+      if(routeCategory) {
+        this.events = this.events?.filter(event => event.category === routeCategory);
+      }
+    });
+  }
 }
