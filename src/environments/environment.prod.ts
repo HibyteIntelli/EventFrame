5b15c4088b84efe99990c1bbc6536011dacacf2e
daiana.iurescu <daiana.iurescu@gmail.com>
2022-06-29 12:13:00 +0300

    changed api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   authUrl: 'https://api.dev.si-nect-portal.de:4000/auth/api-key',
-  apiKey: '272ede51-7d74-4b24-bd95-ff0f60d17e3f',
+  apiKey: 'd69b5b1a-b422-4f67-bf32-6a75bcfb5a2c',
   apiUrl: 'https://api.dev.si-nect-portal.de:4000/agv'
 };
