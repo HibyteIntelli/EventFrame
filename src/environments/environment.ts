ea39385351745a4a7cd2948acde9e9c87e80a259
iuliavoit <iuliavoit13@yahoo.com>
2022-07-12 19:01:38 +0300

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  authUrl: 'https://dev.si-nect-api.de/agv/auth/api-key',
+  authUrl: 'https://dev.si-nect-api.de/auth/api-key',
   apiKey: '272ede51-7d74-4b24-bd95-ff0f60d17e3f',
   apiUrl: 'https://dev.si-nect-api.de/agv'
 };
