1f52cbf0f6cee722628b3a3f80ff73874adbf178
iuliavoit <iuliavoit13@yahoo.com>
2022-07-12 13:47:12 +0300

    url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authUrl: 'https://api.dev.si-nect-portal.de:4000/auth/api-key',
+  authUrl: 'https://api.dev.si-nect-portal.de/auth/api-key',
   apiKey: 'd69b5b1a-b422-4f67-bf32-6a75bcfb5a2c',
-  apiUrl: 'https://api.dev.si-nect-portal.de:4000/agv'
+  apiUrl: 'https://api.dev.si-nect-api.de'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  authUrl: 'https://dev.si-nect-api.de/agv/auth/api-key',
+  authUrl: 'https://dev.si-nect-api.de/auth/api-key',
   apiKey: '272ede51-7d74-4b24-bd95-ff0f60d17e3f',
-  apiUrl: 'https://dev.si-nect-api.de/agv'
+  apiUrl: 'https://dev.si-nect-api.de'
 };
 
 /*
