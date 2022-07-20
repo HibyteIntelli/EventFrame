ce094950a597087ea03758fdf7f9a188cc871e88
alexbinzar1 <alex.binzar@hibyte.ro>
2022-07-20 11:03:11 +0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authUrl: 'https://si-nect-api.de/auth/api-key',
+  authUrl: 'https://dev.si-nect-api.de/auth/api-key',
   apiKey: 'd69b5b1a-b422-4f67-bf32-6a75bcfb5a2c',
-  apiUrl: 'https://si-nect-api.de/agv'
+  apiUrl: 'https://dev.si-nect-api.de/agv'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  authUrl: 'https://si-nect-api.de/auth/api-key',
+  authUrl: 'https://dev.si-nect-api.de/auth/api-key',
   apiKey: 'd69b5b1a-b422-4f67-bf32-6a75bcfb5a2c',
-  apiUrl: 'https://si-nect-api.de/agv'
+  apiUrl: 'https://dev.si-nect-api.de/agv'
 };
 
 /*
